5d901e1336e464164255a3e7113b087af5195875
Pankaj Kumar <pk7zuva@users.noreply.github.com>
2016-05-15 21:36:52 -0400

    Update FromBamExtractProperpairreads2Island2Inetrsect2MappedUnmapped4human.sh
    This script generates microDNA island (a stretch of continuously sequenced bases on Chromosome) from the sorted bam file.
    #Usage: <Program-Name> <Bam-file> <Length-of-longest-chromosome> <length-of-read> <Merge-Island-length (separation between two sequenced bases to be considered part of one island)>

--- FromBamExtractProperpairreads2Island2Inetrsect2MappedUnmapped4human.sh
+++ FromBamExtractProperpairreads2Island2Inetrsect2MappedUnmapped4human.sh
@@ -1,6 +1,6 @@
 #This program is to generate microDNA island from the sorted bam file
 #Usage: Program-Name Bam-file Length-of-longest-chromososme length-of-read Merge-Island-length(separation between two sequenced bases to be considered part of one island)
-ulimit -s unlimited
+ulimit -s unlimited 
 #This loop is specific to human genome
 for i in chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY
         do
